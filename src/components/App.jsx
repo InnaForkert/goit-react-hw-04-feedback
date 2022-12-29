142dcd0bee8ab792b3d919d50bf8db85d7a526bc
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-12-29 18:49:57 +0200

    to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,55 +3,52 @@ import Options from './Options';
 import Statistics from './Statistics';
 import Section from './Section';
 import Notification from './Notification';
+import { useState } from 'react';
 
-class App extends Component {
-  state = {
+const App = () => {
+  const [feedbackCount, setFeedbackCount] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
+  });
+
+  const updateRes = e => {
+    setFeedbackCount(prevState => {
+      return { ...prevState, [e.targer.name]: prevState[e.target.name] + 1 };
+    });
   };
 
-  updateRes = e => {
-    this.setState(prevState => ({
-      [e.target.name]: prevState[e.target.name] + 1,
-    }));
-  };
-
-  countTotalFeedback = () => {
-    return Object.keys(this.state).reduce((acc, el) => acc + this.state[el], 0);
-  };
-  render() {
-    const options = Object.keys(this.state);
-    const results = Object.values(this.state);
-    return (
-      <>
-        <Section
-          flex={true}
-          title={'Please leave feedback'}
-          children={
-            <Options
-              options={options}
-              onLeaveFeedback={this.updateRes.bind(this)}
-            />
-          }
-        />
-        <Section
-          title={'Statistics'}
-          children={
-            this.countTotalFeedback() ? (
-              <Statistics
-                flex={false}
-                options={options}
-                results={results}
-                total={this.countTotalFeedback()}
-              />
-            ) : (
-              <Notification message="There is no feedback"></Notification>
-            )
-          }
-        />
-      </>
+  const countTotalFeedback = () => {
+    return Object.keys(feedbackCount).reduce(
+      (acc, el) => acc + feedbackCount[el],
+      0
     );
-  }
-}
+  };
+  const options = Object.keys(feedbackCount);
+  const results = Object.values(feedbackCount);
+  return (
+    <>
+      <Section
+        flex={true}
+        title={'Please leave feedback'}
+        children={<Options options={options} onLeaveFeedback={updateRes} />}
+      />
+      <Section
+        title={'Statistics'}
+        children={
+          countTotalFeedback() ? (
+            <Statistics
+              flex={false}
+              options={options}
+              results={results}
+              total={countTotalFeedback()}
+            />
+          ) : (
+            <Notification message="There is no feedback"></Notification>
+          )
+        }
+      />
+    </>
+  );
+};
 export default App;
